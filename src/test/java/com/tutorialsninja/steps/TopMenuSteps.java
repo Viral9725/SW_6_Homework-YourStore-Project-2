49ff1bf098e994b32d5a0e0788d4a87decf82370
viral9725 <viral.kakadiya@gmail.com>
2023-05-16 22:23:21 +0100

    Week15 Viral Kakadiya SW_6_Homework-YourStore-Project-2

---
+++ src/test/java/com/tutorialsninja/steps/TopMenuSteps.java
@@ -0,0 +1,51 @@
+package com.tutorialsninja.steps;
+
+import com.tutorialsninja.pages.ComponentsPage;
+import com.tutorialsninja.pages.DesktopsPage;
+import com.tutorialsninja.pages.HomePage;
+import com.tutorialsninja.pages.LaptopsAndNoteBooksPage;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.testng.Assert;
+
+public class TopMenuSteps {
+    @Given("^I am on home page$")
+    public void iAmOnHomePage() {
+    }
+    @When("^I mouse hover on Desktop$")
+    public void iMouseHoverOnDesktop() {
+        new HomePage().mouseHoverOnDesktopsLinkAndClick();
+    }
+
+    @And("^I click on \"([^\"]*)\"$")
+    public void iClickOn(String menu) {
+        new HomePage().selectMenu(menu);
+    }
+
+    @Then("^Verify that User is navigated to Desktop page using \"([^\"]*)\"$")
+    public void verifyThatUserIsNavigatedToDesktopPageUsing(String expected) {
+        Assert.assertEquals(new DesktopsPage().getDesktopsText(),expected,"User in not navigated");
+    }
+
+    @When("^I mouse hover on Laptop and Notebook$")
+    public void iMouseHoverOnLaptopAndNotebook() {
+        new HomePage().mouseHoverOnLaptopsAndNotebooksLinkAndClick();
+    }
+
+    @Then("^Verify that User is navigated to Laptop page using \"([^\"]*)\"$")
+    public void verifyThatUserIsNavigatedToLaptopPageUsing(String expected) {
+        Assert.assertEquals(new LaptopsAndNoteBooksPage().getLaptopsAndNotebooksText(),expected,"User is not navigated");
+    }
+
+    @When("^I mouse hover on Component$")
+    public void iMouseHoverOnComponent() {
+        new HomePage().mouseHoverOnComponentLinkAndClick();
+    }
+
+    @Then("^Verify that User is navigated to Component page using \"([^\"]*)\"$")
+    public void verifyThatUserIsNavigatedToComponentPageUsing(String expected) {
+        Assert.assertEquals(new ComponentsPage().getComponentsText(),expected,"User is not navigated");
+    }
+}
